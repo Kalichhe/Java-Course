d767f5f92617c01b7bacd415b6658f5f0e2fe8fc
Kalichhe <jhondh2003@gmail.com>
2024-01-23 14:16:27 -0500

    Iniciando

---
+++ Variables/UpdatingVariables.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class UpdatingVariables {
+    public static void main(String[] args) {
+
+        int salary = 1000;
+
+        // Gano un bonp
+        int bond = 200;
+
+        // New salary
+        salary = salary + bond;
+        System.out.println(salary);
+    }
+}
---
+++ Variables/Variables.java
@@ -0,0 +1,21 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Tipo de dato, nombre de la variable y lo que se le asigna a la variables
+        // Integer
+        // Inicializar una variable y asignarle un valor
+        int speed = 10;
+        System.out.println(speed);
+
+        // inicializar una variable sin asignarle un valor
+        int salary;
+        // Asignarle un valor a la variable
+        salary = 1000;
+        System.out.println(salary);
+
+        // String
+        String name = "Carlos";
+        System.out.println(name);
+    }
+}
